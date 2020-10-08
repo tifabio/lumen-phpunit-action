cc6e2546c4b91ac54d7e3293be422af0064ff0af
tifabio <tifabio@gmail.com>
2020-10-08 17:01:32 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,5 @@ composer install --prefer-dist --no-ansi --no-interaction --no-progress --no-scr
 cp .env.example .env
 
 php artisan cache:clear
-php artisan migrate:fresh --seed
 
 php vendor/bin/phpunit
